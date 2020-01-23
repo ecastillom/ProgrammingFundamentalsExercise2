6b45791163c004aa526a1ea8e9847377587b291f
ecastillom <59704965+ecastillom@users.noreply.github.com>
2020-01-23 15:17:17 -0800

    Add files via upload

---
+++ Programa1.java
@@ -0,0 +1,23 @@
+public class Programa1
+{
+
+    public static void main(String args[])
+    {
+        //Code
+
+        // Variables DataType NameVariable = Value;
+
+        float Num1 = 5;
+        float Num2 = 10;
+
+
+        System.out.println("The Result is : " + (Num1 + Num2));
+        System.out.println("The Sustraction is : " + (Num1 - Num2));
+        System.out.println("The Multiplication is : " + (Num1 * Num2));
+        System.out.println("The Division is : " + (Num2 / Num1));
+        System.out.println("The Division is : " + (Num1 / Num2));
+
+
+    }
+
+}
